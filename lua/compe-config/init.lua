af3b0e3f3a8ab218ed063dd483e847a83c8177f0
Eric Hansen <ehansen@edcor.com>
2021-07-14 14:28:24 -0400

    Return properly

--- lua/compe-config/init.lua
+++ lua/compe-config/init.lua
@@ -39,7 +39,7 @@ vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
-return {
+require'compe'.setup {
   enabled = true;
   autocomplete = true;
   debug = false;
