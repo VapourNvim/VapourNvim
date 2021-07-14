4007745ba01d98905262103da5986979d4a06433
Eric Hansen <ehansen@edcor.com>
2021-07-14 16:36:39 -0400

    Remove unnecessary if statement for colorizer

--- lua/colorizer-config/init.lua
+++ lua/colorizer-config/init.lua
@@ -1,3 +1,1 @@
-if not Vapour.plugins.colorizer.enabled then return end
-
 require'colorizer'.setup({'*'}, {RGB = true, RRGGBB = true, names = true, RRGGBBAA = true, rgb_fn = true, hsl_fn = true, css = true, css_fn = true})
