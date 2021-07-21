c46107c902ce4563177a19f6d584193f0e85a2af
Eric Hansen <ehansen@edcor.com>
2021-07-21 09:56:04 -0400

    Remove vsnip functions from compe if vsnip is not enabled

--- lua/compe-config/init.lua
+++ lua/compe-config/init.lua
@@ -18,7 +18,7 @@ end
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
+  elseif Vapour.plugins.vsnip.enabled and vim.fn.call("vsnip#available", {1}) == 1 then
     return t "<Plug>(vsnip-expand-or-jump)"
   elseif check_back_space() then
     return t "<Tab>"
@@ -29,7 +29,7 @@ end
 _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+  elseif Vapour.plugins.vsnip.enabled and vim.fn.call("vsnip#jumpable", {-1}) == 1 then
     return t "<Plug>(vsnip-jump-prev)"
   else
     return t "<S-Tab>"
