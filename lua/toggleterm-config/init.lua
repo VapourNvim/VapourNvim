2c5463e43c849894c9d22f806d913e62429db0ae
Neil Sabde <neilsabde@gmail.com>
2021-07-24 12:11:19 +0530

    Update ToggleTerm floating terminals

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -8,17 +8,3 @@ require"toggleterm".setup {
     persist_size = true,
     direction = 'horizontal'
 }
-local Terminal = require('toggleterm.terminal').Terminal
-local float = Terminal:new({direction = "float"})
-local lazygit = Terminal:new({cmd = "lazygit", direction = "float"})
-
-function _float_toggle()
-    float:toggle()
-end
-
-function _lazygit_toggle()
-    lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua _float_toggle()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>tl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
--- lua/vapour/init.lua
+++ lua/vapour/init.lua
@@ -2,100 +2,72 @@ require('vapour.globals')
 
 -- Vapour object to use for namespacing
 Vapour = {
-  language_servers = {
-    sumneko = {
-      enabled = false,
+    language_servers = {sumneko = {enabled = false}},
+    plugins = {
+        colorizer = {enabled = false},
+        colorbuddy = {enabled = false},
+        bufferline = {enabled = false},
+        galaxyline = {enabled = false},
+        dashboard = {enabled = false},
+        toggleterm = {
+            enabled = false,
+            Terminal = require('toggleterm.terminal').Terminal,
+            toggle_float = function()
+                local float = Vapour.plugins.toggleterm.Terminal:new({direction = "float"})
+                return float:toggle()
+            end,
+            toggle_lazygit = function()
+                local lazygit = Vapour.plugins.toggleterm.Terminal:new({cmd = "lazygit", direction = "float"})
+                return lazygit:toggle()
+            end,
+            which_key = {
+                root = "T",
+                name = "Terminal",
+                definitions = {
+                    t = {":ToggleTerm<cr>", "Split Below"},
+                    f = {"<cmd>lua Vapour.plugins.toggleterm.toggle_float()<cr>", "Floating Terminal"},
+                    l = {"<cmd>lua Vapour.plugins.toggleterm.toggle_lazygit()<cr>", "LazyGit"}
+                    -- l = {"<cmd>lua require('lspsaga.floaterm').open_float_terminal('lazygit')<cr>", "LazyGit"}
+                }
+            }
+        },
+        treesitter = {enabled = false},
+        rainbow_parentheses = {enabled = false},
+        closetag = {enabled = false},
+        vsnip = {enabled = false},
+        markdown_preview = {eanbled = false},
+        telescope = {enabled = false},
+        nvim_tree = {enabled = false},
+        dial = {enabled = false},
+        format = {enabled = false},
+        nvim_comment = {enabled = false},
+        compe = {enabled = false, sources = {}},
+        which_key = {
+            user_defined = {},
+
+            -- Set to true to allow which_key.user_defined to override anything set by plugins
+            allow_override_mappings = nil
+        },
+
+        -- Packer-specific needs
+        packer = {
+            enabled = true,
+            -- See vapour/user-config/README.md for an example use of this
+            init = {}
+        },
+
+        -- User-loaded plugins
+        user = {}
+    },
+
+    -- To update see vapour.utils
+    utils = {},
+
+    settings = {
+        -- If true, :w -> :w!
+        always_force_write = false,
+        colorscheme = 'onedark'
     }
-  },
-  plugins = {
-    colorizer = {
-      enabled = false,
-    },
-    colorbuddy = {
-      enabled = false,
-    },
-    bufferline = {
-      enabled = false,
-    },
-    galaxyline = {
-      enabled = false,
-    },
-    dashboard = {
-      enabled = false,
-    },
-    toggleterm = {
-      enabled = false,
-      which_key = {
-        root = "T",
-        name = "Terminal",
-        definitions = {
-          t = {":ToggleTerm<cr>", "Split Below"},
-          -- f = {"<cmd>lua require('lspsaga.floaterm').open_float_terminal()<cr>", "Floating Terminal"},
-          -- l = {"<cmd>lua require('lspsaga.floaterm').open_float_terminal('lazygit')<cr>", "LazyGit"}
-        }
-      }
-    },
-    treesitter = {
-      enabled = false,
-    },
-    rainbow_parentheses = {
-      enabled = false,
-    },
-    closetag = {
-      enabled = false,
-    },
-    vsnip = {
-      enabled = false,
-    },
-    markdown_preview = {
-      eanbled = false,
-    },
-    telescope = {
-      enabled = false,
-    },
-    nvim_tree = {
-      enabled = false,
-    },
-    dial = {
-      enabled = false,
-    },
-    format = {
-      enabled = false,
-    },
-    nvim_comment = {
-      enabled = false,
-    },
-    compe = {
-      enabled = false,
-      sources = {},
-    },
-    which_key = {
-      user_defined = {},
-
-      -- Set to true to allow which_key.user_defined to override anything set by plugins
-      allow_override_mappings = nil,
-    },
-
-    -- Packer-specific needs
-    packer = {
-      enabled = true,
-      -- See vapour/user-config/README.md for an example use of this
-      init = {
-      },
-    },
-
-    -- User-loaded plugins
-    user = {},
-  },
-
-  -- To update see vapour.utils
-  utils = {},
-
-  settings = {
-    -- If true, :w -> :w!
-    always_force_write = false,
-    colorscheme = 'onedark',
-  }
 }
 
 require('vapour.utils')
