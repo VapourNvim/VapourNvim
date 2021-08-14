339ca94981931355505239b52b27771ce6901529
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2021-08-14 10:38:36 +0530

    Center packer window

--- lua/vapour/init.lua
+++ lua/vapour/init.lua
@@ -6,34 +6,46 @@ Vapour = {}
 Vapour = {
     options = {tabwidth = 2},
     language_servers = {
-      sumneko = {enabled = false}, 
-      bash = {enabled = false}, 
-      cssls = {enabled = false}, 
-      html = {enabled = false, vapour_init = function()
-          local lspconfig = Vapour.utils.plugins.require('lspconfig')
+        sumneko = {enabled = false},
+        bash = {enabled = false},
+        cssls = {enabled = false},
+        html = {
+            enabled = false,
+            vapour_init = function()
+                local lspconfig = Vapour.utils.plugins.require('lspconfig')
 
-          Vapour.language_servers.html.setup.capabilities = vim.lsp.protocol.make_client_capabilities()
-          Vapour.language_servers.html.setup.capabilities.textDocument.completion.completionItem.snippetSupport = true
-        end,
-        setup = {}},
-      tsserver = {enabled = false}, 
-      pyright = {enabled = false}, 
-      jedi_language_server = {enabled = false}, 
-      pylsp = {enabled = false}, 
-      vimls = {enabled = false}, 
-      yamlls = {enabled = false}, 
-      solargraph = {enabled = false}, 
-      vuels = {enabled = false}, 
-      phpactor = {enabled = false, vapour_init = function() require'language-servers.phpactor' end}, 
-      jsonls = {enabled = false, setup = {commands = {
-        Format = {
-            function()
-                vim.lsp.buf.range_formatting({}, {0, 0}, {vim.fn.line("$"), 0})
+                Vapour.language_servers.html.setup.capabilities = vim.lsp.protocol.make_client_capabilities()
+                Vapour.language_servers.html.setup.capabilities.textDocument.completion.completionItem.snippetSupport = true
+            end,
+            setup = {}
+        },
+        tsserver = {enabled = false},
+        pyright = {enabled = false},
+        jedi_language_server = {enabled = false},
+        pylsp = {enabled = false},
+        vimls = {enabled = false},
+        yamlls = {enabled = false},
+        solargraph = {enabled = false},
+        vuels = {enabled = false},
+        phpactor = {
+            enabled = false,
+            vapour_init = function()
+                require 'language-servers.phpactor'
             end
-          }
-        }
-      }}, 
-      gopls = {enabled = false, setup = {cmd = {"gopls", "serve"}, settings = {gopls = {analyses = {unusedparams = true}, staticcheck = true}}}},
+        },
+        jsonls = {
+            enabled = false,
+            setup = {
+                commands = {
+                    Format = {
+                        function()
+                            vim.lsp.buf.range_formatting({}, {0, 0}, {vim.fn.line("$"), 0})
+                        end
+                    }
+                }
+            }
+        },
+        gopls = {enabled = false, setup = {cmd = {"gopls", "serve"}, settings = {gopls = {analyses = {unusedparams = true}, staticcheck = true}}}}
     },
     plugins = {
         colorizer = {enabled = false},
@@ -44,12 +56,12 @@ Vapour = {
         toggleterm = {
             enabled = false,
             toggle_float = function()
-            	local Terminal = require('toggleterm.terminal').Terminal
+                local Terminal = require('toggleterm.terminal').Terminal
                 local float = Terminal:new({direction = "float"})
                 return float:toggle()
             end,
             toggle_lazygit = function()
-            	local Terminal = require('toggleterm.terminal').Terminal
+                local Terminal = require('toggleterm.terminal').Terminal
                 local lazygit = Terminal:new({cmd = "lazygit", direction = "float"})
                 return lazygit:toggle()
             end,
@@ -75,15 +87,19 @@ Vapour = {
         format = {enabled = false},
         nvim_comment = {enabled = false},
         compe = {enabled = false, sources = {}},
-        which_key = {
-            user_defined = {},
-        },
+        which_key = {user_defined = {}},
 
         -- Packer-specific needs
         packer = {
             enabled = true,
             -- See vapour/user-config/README.md for an example use of this
-            init = {}
+            init = {
+                display = {
+                    open_fn = function()
+                        return Vapour.utils.plugins.require('packer.util').float {border = "single"}
+                    end
+                }
+            }
         },
 
         -- User-loaded plugins
