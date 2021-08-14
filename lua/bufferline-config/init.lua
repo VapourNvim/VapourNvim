8b6a6db4cfbbbd9ec3f4f4d3e2ed9f81a183d975
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2021-08-14 11:14:10 +0530

    Add bufferline config with offsets

---
+++ lua/bufferline-config/init.lua
@@ -0,0 +1,1 @@
+require"bufferline".setup {options = {offsets = {{filetype = "NvimTree", text = "File Explorer", highlight = "Directory", text_align = "center"}}}}
--- lua/vapour/plugins/init.lua
+++ lua/vapour/plugins/init.lua
@@ -13,7 +13,7 @@ local packer = Vapour.utils.plugins.require('packer')
 packer.init(Vapour.plugins.packer.init)
 
 local function is_enabled(plugin)
-  return Vapour.plugins[plugin].enabled
+    return Vapour.plugins[plugin].enabled
 end
 
 return packer.startup(function(use)
@@ -21,13 +21,28 @@ return packer.startup(function(use)
     -- Syntax Highlighting and Visual Plugins
     use {'norcalli/nvim-colorizer.lua', disable = not is_enabled('colorizer'), config = "require'colorizer-config'"}
     use {'tjdevries/colorbuddy.nvim', disable = not is_enabled('colorbuddy')}
-    use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons', config = "require'bufferline'.setup{}", disable = not is_enabled('bufferline')}
+    use {
+        'akinsho/nvim-bufferline.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = 'require"bufferline-config"',
+        disable = not is_enabled('bufferline')
+    }
     use {'glepnir/galaxyline.nvim', branch = 'main', disable = not is_enabled('galaxyline'), config = "require'galaxyline-config'"}
     use {'glepnir/dashboard-nvim', disable = not is_enabled('dashboard'), config = "require'dashboard-config'"}
 
     -- Tree-Sitter
-    use {'nvim-treesitter/nvim-treesitter', event = 'BufRead', run = ':TSUpdate', disable = not is_enabled('treesitter'), config = "require'treesitter-config'"}
-    use {'junegunn/rainbow_parentheses.vim', disable = not is_enabled('rainbow_parentheses'), config = "vim.cmd('autocmd FileType * RainbowParentheses')"}
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        event = 'BufRead',
+        run = ':TSUpdate',
+        disable = not is_enabled('treesitter'),
+        config = "require'treesitter-config'"
+    }
+    use {
+        'junegunn/rainbow_parentheses.vim',
+        disable = not is_enabled('rainbow_parentheses'),
+        config = "vim.cmd('autocmd FileType * RainbowParentheses')"
+    }
     use {'alvan/vim-closetag', disable = not is_enabled('closetag'), config = "require'closetag-config'"}
 
     -- Colorschemes
@@ -39,7 +54,14 @@ return packer.startup(function(use)
     -- LSP and Autocomplete
     use {'neovim/nvim-lspconfig', event = "BufRead"}
     use 'glepnir/lspsaga.nvim'
-    use {'hrsh7th/nvim-compe', event = "InsertEnter *", config = function() require'compe-config' end, disable = not is_enabled('compe')}
+    use {
+        'hrsh7th/nvim-compe',
+        event = "InsertEnter *",
+        config = function()
+            require 'compe-config'
+        end,
+        disable = not is_enabled('compe')
+    }
     use {'hrsh7th/vim-vsnip', disable = not is_enabled('vsnip')}
     use {'windwp/nvim-autopairs', after = {'nvim-compe'}, config = "require'autopairs-config'"}
 
@@ -54,7 +76,12 @@ return packer.startup(function(use)
     use {'akinsho/nvim-toggleterm.lua', disable = not is_enabled('toggleterm'), config = 'require"toggleterm-config"'}
 
     -- Navigation
-    use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}, disable = not is_enabled('telescope'), config = "require'telescope-config'"}
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+        disable = not is_enabled('telescope'),
+        config = "require'telescope-config'"
+    }
     use {'kyazdani42/nvim-tree.lua', cmd = "NvimTreeToggle", disable = not is_enabled('nvim_tree'), config = "require'nvimtree-config'"}
     use 'phaazon/hop.nvim'
 
@@ -64,7 +91,5 @@ return packer.startup(function(use)
     use {'lukas-reineke/format.nvim', disable = not is_enabled('format'), config = "require'formatting'"}
     use {'folke/which-key.nvim', event = "BufWinEnter"}
 
-    for _, plugin in pairs(Vapour.plugins.user) do
-      use(plugin)
-    end
+    for _, plugin in pairs(Vapour.plugins.user) do use(plugin) end
 end)
