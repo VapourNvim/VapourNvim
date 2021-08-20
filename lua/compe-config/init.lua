4eef5adf953e31740f5efccc54e9b3252cce4a1c
hackorum <neilsabde@gmail.com>
2021-08-20 14:20:29 +0530

    Add vsnip to compe sources

--- lua/compe-config/init.lua
+++ lua/compe-config/init.lua
@@ -3,7 +3,7 @@ Vapour.utils.plugins.packadd('nvim-compe')
 vim.o.completeopt = "menuone,noselect,noinsert"
 
 local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
@@ -16,24 +16,24 @@ local check_back_space = function()
 end
 
 _G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif Vapour.plugins.vsnip.enabled and vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif Vapour.plugins.vsnip.enabled and vim.fn.call("vsnip#available", {1}) == 1 then
+        return t "<Plug>(vsnip-expand-or-jump)"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn['compe#complete']()
+    end
 end
 _G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif Vapour.plugins.vsnip.enabled and vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
-  else
-    return t "<S-Tab>"
-  end
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif Vapour.plugins.vsnip.enabled and vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+        return t "<Plug>(vsnip-jump-prev)"
+    else
+        return t "<S-Tab>"
+    end
 end
 
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
@@ -41,30 +41,23 @@ vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
-compe_sources = {
-  nvim_lsp = true,
-  spell = true,
-  tags = true,
-  treesitter = Vapour.plugins.treesitter.enabled,
-}
+compe_sources = {nvim_lsp = true, spell = true, tags = true, treesitter = Vapour.plugins.treesitter.enabled, vsnip = Vapour.plugins.vsnip.enabled}
 
-for source, opts in pairs(Vapour.plugins.compe.sources) do
-  compe_sources[source] = opts
-end
+for source, opts in pairs(Vapour.plugins.compe.sources) do compe_sources[source] = opts end
 
 require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
+    enabled = true,
+    autocomplete = true,
+    debug = false,
+    min_length = 1,
+    preselect = 'enable',
+    throttle_time = 80,
+    source_timeout = 200,
+    incomplete_delay = 400,
+    max_abbr_width = 100,
+    max_kind_width = 100,
+    max_menu_width = 100,
+    documentation = true,
 
-  source = compe_sources;
+    source = compe_sources
 }
