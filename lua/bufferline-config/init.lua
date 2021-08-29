b81531d5bfcbed0cef5c2e0db096b9516adc415c
hackorum <neilsabde@gmail.com>
2021-08-29 12:28:38 +0530

    Remove bufferline text

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -1,1 +1,1 @@
-require"bufferline".setup {options = {offsets = {{filetype = "NvimTree", text = "File Explorer", highlight = "Directory", text_align = "center"}}}}
+require"bufferline".setup {options = {offsets = {{filetype = "NvimTree", highlight = "Directory", text_align = "center"}}}}
