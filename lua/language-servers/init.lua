7d0009fda25537d10efa22babae91a174724455c
hackorum <neilsabde@gmail.com>
2021-09-11 13:10:34 +0530

    Update language server on insert

--- lua/language-servers/init.lua
+++ lua/language-servers/init.lua
@@ -99,3 +99,9 @@ require('vim.lsp.protocol').CompletionItemKind = {
     '', '', 'ƒ', ' ', '', '', '', 'ﰮ', '', '', '', '', '了', ' ', '﬌ ', ' ', ' ', '', ' ', ' ',
     ' ', ' ', '', '', '<>'
 }
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = {spacing = 5, severity_limit = 'Warning'},
+    update_in_insert = true
+})
