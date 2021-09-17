95174998f013899e24c5b5327c5437d86a1e7ee1
hackorum <neilsabde@gmail.com>
2021-09-17 20:58:28 +0530

    add treesitter indent

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -3,6 +3,7 @@ Vapour.utils.plugins.packadd('nvim-treesitter')
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = {"haskell"},
+    indent = {enable = true},
     highlight = {enable = true},
     autotag = {enable = Vapour.plugins.closetag.enabled},
     rainbow = {enable = Vapour.plugins.rainbow_parentheses.enabled, extended_mode = false, disable = {"html"}},
