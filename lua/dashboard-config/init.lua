24fd8cb475f342f71971a6b851ba8b6d29477596
Neil Sabde <neilsabde@gmail.com>
2021-10-31 09:49:42 +0530

    Update config file for dashboard

--- lua/dashboard-config/init.lua
+++ lua/dashboard-config/init.lua
@@ -13,7 +13,7 @@ vim.g.dashboard_custom_section = {
     a = {description = {'  Find File          '}, command = 'Telescope find_files'},
     d = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
     b = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
-    e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'},
+    e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/lua/vapour/user-config/init.lua'},
     f = {description = {'  Git                '}, command = 'LazyGit'}
 }
 vim.g.dashboard_custom_footer = {'Do one thing, do it well - Unix philosophy'}
