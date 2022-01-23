f029deefdba9ba3f3f885c89e6cd3ec2a2c03335
Neil Sabde <neilsabde@gmail.com>
2022-01-23 12:57:27 +0530

    Update stabline config

--- lua/staline-config/stabline.lua
+++ lua/staline-config/stabline.lua
@@ -1,15 +1,11 @@
 require('stabline').setup {
-  defaults = {
-    style = "bar",
-    stab_left = "┃",
-    stab_right = " ",
-    inactive_bg = "#1e2127",
-    inactive_fg = "#aaaaaa",
-    -- stab_bg     = Default is darker version of bg.,
-
-    font_active = "bold",
-    exclude_fts = {'NvimTree', 'dashboard', 'lir'},
-    stab_start = "",
-    stab_end = ""
-  }
+  style = "bar",
+  stab_left = "┃",
+  stab_right = " ",
+  inactive_bg = "#232136",
+  inactive_fg = "#FAF4ED",
+  font_active = "bold",
+  exclude_fts = {'NvimTree', 'dashboard', 'lir'},
+  stab_start = "",
+  stab_end = ""
 }
