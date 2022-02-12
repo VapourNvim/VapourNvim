c56489c69214c09bb1adf1889ca3f70947b8568f
quynguyen <quy.nguyen@gmail.com>
2022-02-11 21:17:53 -0800

    Including the telescope extension 'fzf'

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -38,6 +38,16 @@ telescope.setup {
         ["<C-k>"] = actions.move_selection_previous,
         ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
       }
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,                    -- false will only do exact matching
+        override_generic_sorter = true,  -- override the generic sorter
+        override_file_sorter = true,     -- override the file sorter
+        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+	                                -- the default case_mode is "smart_case"
+      } 
     }
   }
 }
+telescope.load_extension 'fzf'
