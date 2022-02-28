efbfd2a8800693ce0b96756181f79d405a839376
Bhushan Gaikwad <43483545+bhushan@users.noreply.github.com>
2022-02-28 14:17:51 +0530

    Update rose-pine.lua
    Recently, settings got updated.

--- lua/colorscheme/rose-pine.lua
+++ lua/colorscheme/rose-pine.lua
@@ -1,4 +1,4 @@
 Vapour.utils.plugins.packadd('rose-pine')
-vim.g.rose_pine_disable_italics = true
+vim.g.disable_italics = true
 vim.cmd('colorscheme rose-pine')
 Vapour.settings.lualine_colorscheme = "rose-pine"
