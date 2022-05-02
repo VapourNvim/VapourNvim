b82d3f1136a54d716e4954d5b6dc34c72930594b
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2022-05-02 14:59:39 +0530

    Fix nvim tree indent markers

--- lua/nvimtree-config/init.lua
+++ lua/nvimtree-config/init.lua
@@ -1,8 +1,8 @@
-vim.g.nvim_tree_indent_markers = 1
 local nvimtree = Vapour.utils.plugins.require 'nvim-tree'
 nvimtree.setup {
   filters = {custom = {'*.tmp', '.git'}},
   disable_netrw = true,
+  indent_markers = 1,
   hijack_netrw = true,
   open_on_setup = true,
   ignore_ft_on_setup = {'dashboard'},
@@ -20,8 +20,10 @@ nvimtree.setup {
   view = {
     width = Vapour.plugins.nvim_tree.view_width,
     side = 'left',
-    auto_resize = true,
     mappings = {custom_only = false, list = {}}
+  },
+  renderer = {
+    indent_markers = {enable = true, icons = {corner = "└ ", edge = "│ ", none = "  "}}
   }
 }
 vim.cmd [[
--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -2,8 +2,16 @@ local actions = Vapour.utils.plugins.require('telescope.actions')
 local telescope = Vapour.utils.plugins.require('telescope')
 telescope.setup {
   defaults = {
-    layout_config = {width = 0.75, prompt_position = "top", preview_cutoff = 120, horizontal = {mirror = false}, vertical = {mirror = false}},
-    find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+    layout_config = {
+      width = 0.75,
+      prompt_position = "top",
+      preview_cutoff = 120,
+      horizontal = {mirror = false},
+      vertical = {mirror = false}
+    },
+    find_command = {
+      'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'
+    },
     prompt_prefix = " ",
     selection_caret = " ",
     entry_prefix = "  ",
@@ -41,12 +49,11 @@ telescope.setup {
     },
     extensions = {
       fzf = {
-        fuzzy = true,                    -- false will only do exact matching
-        override_generic_sorter = true,  -- override the generic sorter
-        override_file_sorter = true,     -- override the file sorter
-        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-	                                -- the default case_mode is "smart_case"
-      } 
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+      }
     }
   }
 }
