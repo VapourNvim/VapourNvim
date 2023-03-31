ac4a4091b33fbcffc38d164251eb545b561e594b
Traap <gary.a.howard@mac.com>
2023-03-31 06:30:54 -0700

    Remove Youtube comment.

--- lua/nvimtree-config/init.lua
+++ lua/nvimtree-config/init.lua
@@ -1,36 +1,32 @@
 -- Bootstrap: Bail during initialization.
-if vim.g.nvim_bootsrapped == 1 then return nil end
+if vim.g.nvim_bootsrapped == 1 then
+  return nil
+end
 
-local nvimtree = Vapour.utils.plugins.require 'nvim-tree'
-nvimtree.setup {
-  filters = {custom = {'*.tmp', '.git'}},
+local nvimtree = Vapour.utils.plugins.require("nvim-tree")
+nvimtree.setup({
+  filters = { custom = { "*.tmp", ".git" } },
   disable_netrw = true,
   hijack_netrw = true,
-
--- Youtube: Deprecated
---  open_on_setup = true,
---  ignore_ft_on_setup = {'dashboard'},
---  open_on_tab = false,
-
   hijack_cursor = true,
   update_cwd = true,
-  update_focused_file = {enable = true, update_cwd = true, ignore_list = {}},
+  update_focused_file = { enable = true, update_cwd = true, ignore_list = {} },
   system_open = {
     -- the command to run this, leaving nil should work in most cases
     cmd = nil,
     -- the command arguments as a list
-    args = {}
+    args = {},
   },
-  filters = {custom = {'.git'}},
+  filters = { custom = { ".git" } },
   view = {
     width = Vapour.plugins.nvim_tree.view_width,
-    side = 'left',
-    mappings = {custom_only = false, list = {}}
+    side = "left",
+    mappings = { custom_only = false, list = {} },
   },
   renderer = {
-    indent_markers = {enable = true, icons = {corner = "└ ", edge = "│ ", none = "  "}}
-  }
-}
-vim.cmd [[
+    indent_markers = { enable = true, icons = { corner = "└ ", edge = "│ ", none = "  " } },
+  },
+})
+vim.cmd([[
 autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
-]]
+]])
